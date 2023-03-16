bd6c9f141d7e3c4ea20114cc26bf540f9fb1d910
aaidarov-triplea <aaidarov.triplea@gmail.com>
2023-03-16 21:14:53 +0600

    get from back json response

--- src/page/block/item.jsx
+++ src/page/block/item.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 
 export const BlockItemPage = () => {
+    const [post, setPost] = useState(null)
+    const { id } = useParams()
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then(res => res.json())
+        .then((data) => {
+            setPost(data)
+        })
+    }, [id])
+
     return (
         <div>
-            <h1>BlockItemPage</h1>
+            <h1>{post?.title}</h1>
+            <p>{post?.body}</p>
         </div>
     )
 }
